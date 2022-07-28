025d200b0e9108ddbe09de747129401ab92b897a
Adit0507 <adi05072002@gmail.com>
2022-07-28 20:26:44 +0530

    Working on Mobile Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
-import {BsFillCartFill} from "react-icons/bs";
+import React, {useState} from "react";
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from "react-icons/ai";
+import { TbTruckDelivery } from "react-icons/tb";
+import { BsFillCartFill, BsFillSaveFill } from "react-icons/bs";
+import {MdFavorite, MdHelp} from "react-icons/md";
+import {FaWallet, FaUserFriends} from "react-icons/fa";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       <div className="flex items-center">
@@ -20,13 +25,18 @@ const Navbar = () => {
       </div>
 
       {/* Search Bar  */}
-      <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
         <AiOutlineSearch size={25} />
-        <input className="bg-transparent p-2 w-full focus:outline-none" type="text" placeholder='Search foods' />
+        <input
+          className="bg-transparent p-2 w-full focus:outline-none"
+          type="text"
+          placeholder="Search foods"
+        />
       </div>
       {/* Cart Button */}
       <button className="bg-black text-white hidden md:flex items-center py-2">
-        <BsFillCartFill size={20} className='mr-2' />Cart
+        <BsFillCartFill size={20} className="mr-2" />
+        Cart
       </button>
 
       {/* Mobile Menu  */}
@@ -34,13 +44,48 @@ const Navbar = () => {
       {/* Overlay  */}
       <div className="bg-black/70 fixed w-full h-screen z-10 top-0 left-0"></div>
 
-    {/* Side drawer menu */}
-      <div className="fixed top-0 left-0 w-[300px] h-screen bg-white">
-
+      {/* Side drawer menu */}
+      <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
+        <AiOutlineClose
+          size={20}
+          className="absolute right-4 top-4 cursor-pointer"
+        />
+        <h2 className="text-2xl p-4">
+          Best <span className="font-bold">Eats</span>{" "}
+        </h2>
+        <nav>
+          <ul className="flex flex-col p-4 text-gray-800">
+            <li className="text-xl py-4 flex">
+              <TbTruckDelivery size={25} className="mr-4" />
+              Orders
+            </li>
+            <li className="text-xl py-4 flex">
+              <MdFavorite size={25} className="mr-4" />
+              Favorites
+            </li>
+            <li className="text-xl py-4 flex">
+              <FaWallet size={25} className="mr-4" />
+              Wallets
+            </li>
+            <li className="text-xl py-4 flex">
+              <MdHelp size={25} className="mr-4" />
+              Help
+            </li>
+            <li className="text-xl py-4 flex">
+              <AiFillTag size={25} className="mr-4" />
+              Promotions
+            </li>
+            <li className="text-xl py-4 flex">
+              <BsFillSaveFill size={25} className="mr-4" />
+              Best One
+            </li>
+            <li className="text-xl py-4 flex">
+              <FaUserFriends size={25} className="mr-4" />
+              Invite Friends
+            </li>
+          </ul>
+        </nav>
       </div>
-
-
-
     </div>
   );
 };
