d5779e61782f7ffd23a1d41ad69cd47bbb432285
Adit0507 <adi05072002@gmail.com>
2022-07-29 14:36:55 +0530

    Working on Food

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { data } from "../data/data";
 
 const Food = () => {
   return (
-    <div>Food</div>
-  )
-}
+    <div className="max-w-[1640px] m-auto px-4 py-12">
+      <h1 className="text-orange-600 font-bold text-4xl text-center">Top Rated Menu Items</h1>
 
-export default Food
+      {/* Filtering Row  */}
+      <div>
+        {/* Filter type */}
+        <div>
+          <p>Filter Type</p>
+          <div>
+            <button>All</button>
+            <button>Burgers</button>
+            <button>Pizza</button>
+            <button>Salads</button>
+            <button>Chicken</button>
+          </div>
+        </div>
+
+        {/* Filtering price */}
+        <div>
+          <p>Filter Price</p>
+          <div>
+            <button>₹</button>
+            <button>₹₹</button>
+            <button>₹₹₹</button>
+            <button>₹₹₹₹</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Food;
